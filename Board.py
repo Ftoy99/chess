6e6fbb11c2d80c2c5405c789c0e2e84fa15c88e3
Ftoy <spit10000@yahoo.gr>
2024-11-08 16:11:50 +0200

    Add move methods to Board

--- Board.py
+++ Board.py
@@ -1,9 +1,8 @@
 class Board:
-    board = []
-
     def __init__(self):
+        self.turn = 1
         self.board = [
-            # H G F E D C B A
+            # h g f e d c b a
             ["R", "N", "B", "Q", "K", "B", "N", "R"],  # 1
             ["P", "P", "P", "P", "P", "P", "P", "P"],  # 2
             [".", ".", ".", ".", ".", ".", ".", "."],  # 3
@@ -14,6 +13,31 @@ class Board:
             ["R", "N", "B", "Q", "K", "B", "N", "R"],  # 8
         ]
 
+    def move(self, move: str):
+        # increase turn counter
+        self.turn += 1
+        if len(move) == 2:
+            self.move_pawn(move)
+        if move.startswith("N"):
+            self.move_knight(move)
+        if move.startswith("B"):
+            self.move_bishop(move)
+        if move.startswith("Q"):
+            self.move_queen(move)
+        if move.startswith("K"):
+            self.move_king(move)
+        if move.startswith("R"):
+            self.move_rook(move)
+        if move == "0-0":
+            self.move_castle_kingside(move)
+        if move == "0-0-0":
+            self.move_castle_queenside(move)
+
+    def isWhiteTurn(self):
+        if self.turn % 1:
+            return True
+        return False
+
     def __str__(self):
         boardStr = ""
         for row in self.board:
@@ -21,6 +45,30 @@ class Board:
                 boardStr += col
             boardStr += "\n"
         return boardStr
+
+    def move_pawn(self, move):
+        pass
+
+    def move_knight(self, move):
+        pass
+
+    def move_bishop(self, move):
+        pass
+
+    def move_queen(self, move):
+        pass
+
+    def move_king(self, move):
+        pass
+
+    def move_rook(self, move):
+        pass
+
+    def move_castle_kingside(self, move):
+        pass
+
+    def move_castle_queenside(self, move):
+        pass
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+from Board import Board
+
 if __name__ == '__main__':
-    pass
+    board = Board()
+    board.move("e4")
